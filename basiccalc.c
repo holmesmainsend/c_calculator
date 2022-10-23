e93b265aee01d7361b98b398a2c74dee251d1a95
Mona Lisa <holmesmainsend@gmail.com>
2022-10-23 12:12:09 -0400

    added operator functionality

--- basiccalc.c
+++ basiccalc.c
@@ -3,12 +3,25 @@
 
 int main() {
     double num1, num2;
+    char op;
+    
     printf("Enter first number: ");
     scanf("%lf", &num1);
     printf("Enter second number: ");
     scanf("%lf", &num2);
+    printf("Enter operator: ");
+    scanf(" %c", &op);
 
-    printf("Answer: %lf\n", num1 + num2);
+    if (op == '+')
+        printf("%lf\n", num1 + num2);
+    else if (op == '-')
+        printf("%lf\n", num1 - num2);
+    else if (op == '*')
+        printf("%lf\n", num1 - num2);
+    else if (op == '/')
+        printf("%lf\n", num1 - num2);
+    else
+        printf("Invalid operator\n");
 
     return 0;
 }
